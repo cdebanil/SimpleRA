f461d39bc1b0da59445b73271f64895ba493546e
Debanil Chowdhury <cdebanil@gmail.com>
2023-09-01 06:48:42 +0530

    Classroom Assignment

--- src/executors/source.cpp
+++ src/executors/source.cpp
@@ -30,5 +30,50 @@ bool semanticParseSOURCE()
 void executeSOURCE()
 {
     logger.log("executeSOURCE");
+
+    std::ifstream queryFile;
+    queryFile.open("../data/" + parsedQuery.sourceFileName + ".ra");
+    if (!queryFile.is_open() ){
+        std::cerr << "Error opening .ra file: " << parsedQuery.sourceFileName << std::endl;
+        return;
+    }
+    regex delim("[^\\s,]+");
+
+    std::string command;
+    while (queryFile) {
+        cout << endl;
+        getline(queryFile, command);
+        if(command.size()<=0){return;}
+
+        tokenizedQuery.clear();
+        parsedQuery.clear();
+        logger.log("\nReading New Command: ");
+        logger.log(command);
+
+        auto words_begin = std::sregex_iterator(command.begin(), command.end(), delim);
+        auto words_end = std::sregex_iterator();
+        for (std::sregex_iterator i = words_begin; i != words_end; ++i)
+            tokenizedQuery.emplace_back((*i).str());
+
+        if (tokenizedQuery.size() == 1 && tokenizedQuery.front() == "QUIT") {
+            break;
+        }
+
+        if (tokenizedQuery.empty()) {
+            continue;
+        }
+
+        if (tokenizedQuery.size() == 1) {
+            cout << "SYNTAX ERROR" << endl;
+            continue;
+        }
+
+        logger.log("doCommand");
+        if (syntacticParse() && semanticParse()) {
+            executeCommand();
+        }
+    }
+    queryFile.close();
+
     return;
 }
